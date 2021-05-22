86fee76b8d41f6a4e0180acf5e8097f0280df9ca
anubhavcu <anubhav342y@gmail.com>
2021-05-22 10:29:26 +0530

    added react-router-dom
    - used react-router to navigating between pages
    - used LinkContainer from React-router-bootstrap in Header component
      instead of plain Link tag

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,4 +1,6 @@
 import { Navbar, Nav, Container } from 'react-bootstrap';
+import { LinkContainer } from 'react-router-bootstrap';
+
 import React from 'react';
 
 const Header = () => {
@@ -6,16 +8,22 @@ const Header = () => {
     <header>
       <Navbar bg='dark' variant='dark' expand='lg' collapseOnSelect>
         <Container>
-          <Navbar.Brand href='/'>The Corner Store!</Navbar.Brand>
+          <LinkContainer to='/'>
+            <Navbar.Brand>The Corner Store!</Navbar.Brand>
+          </LinkContainer>
           <Navbar.Toggle aria-controls='basic-navbar-nav' />
           <Navbar.Collapse id='basic-navbar-nav'>
             <Nav className='ms-auto'>
-              <Nav.Link href='/cart'>
-                <i className='fas fa-shopping-cart m-2'></i>Cart
-              </Nav.Link>
-              <Nav.Link href='/login'>
-                <i className='fas fa-user m-2'></i>Login
-              </Nav.Link>
+              <LinkContainer to='/cart'>
+                <Nav.Link>
+                  <i className='fas fa-shopping-cart m-2'></i>Cart
+                </Nav.Link>
+              </LinkContainer>
+              <LinkContainer to='/login'>
+                <Nav.Link>
+                  <i className='fas fa-user m-2'></i>Login
+                </Nav.Link>
+              </LinkContainer>
             </Nav>
           </Navbar.Collapse>
         </Container>
@@ -25,6 +33,8 @@ const Header = () => {
 };
 
 export default Header;
+
+// LinkContainer is used instead of just Link, both are essentially same, but LinkContainer allows wrapping of React Bootstrap elements
 
 // ml-auto replaced with ms-auto in new version of bootstrap
 // https://stackoverflow.com/a/65254055

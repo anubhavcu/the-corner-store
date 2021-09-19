b391dae549737ba7987e55fb251fd5a900521b5f
anubhavcu <anubhav342y@gmail.com>
2021-09-19 23:07:41 +0530

    connected backend to our mongodb database
    - added /backend/config/db.js file which allows the application to
      connect to the database , this db.js file is invoked in the server.js
    file
    - this connection is established using mongoose, which is added as a
      dependency
    - added userSchema and productSchema and created a User and Product
      model with all the relevant required fields

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,11 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import connectDB from './config/db.js';
 import products from './data/products.js';
 
 dotenv.config();
+
+connectDB();
 
 const app = express();
 
@@ -18,5 +21,8 @@ app.get('/api/products/:id', (req, res) => {
   res.json(product);
 });
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, console.log(`server running on Port ${PORT} `));
+const PORT = process.env.PORT;
+app.listen(
+  PORT,
+  console.log(`server running on Port ${PORT} in mode ${process.env.NODE_ENV}`)
+);

77385549298c6e2c2faa61882dc7ebbccae6739b
anubhavcu <anubhav342y@gmail.com>
2021-09-19 19:55:50 +0530

    changed from common.js to es6 modules in backend
    - added type : module in /package.json allows us to use es6's import
      syntax over require syntax
    - added dotenv dependency
    - minor css changes

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,8 @@
-const express = require('express');
-const products = require('./data/products');
+import express from 'express';
+import dotenv from 'dotenv';
+import products from './data/products.js';
+
+dotenv.config();
 
 const app = express();
 
@@ -15,4 +18,5 @@ app.get('/api/products/:id', (req, res) => {
   res.json(product);
 });
 
-app.listen(5000, console.log('server running on Port 5000'));
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, console.log(`server running on Port ${PORT} `));

6b9134193ee55a27ec5e3c48bf31a3eb5c6a93e4
anubhavcu <anubhav342y@gmail.com>
2021-09-20 20:25:26 +0530

    added Message and Loader components
    - message component will take in variant and a children , variant will
      define the variant of the alert and children will be the contents of
    the alert
    - loader uses Spinner from react-bootstrap

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -3,6 +3,8 @@ import { Col, Row } from 'react-bootstrap';
 import Product from '../components/Product';
 import { useDispatch, useSelector } from 'react-redux';
 import { listProducts } from '../redux/actions/productActions';
+import Loader from '../components/Loader';
+import Message from '../components/Message';
 
 const HomeScreen = () => {
   // const [products, setProducts] = useState([]);
@@ -16,9 +18,9 @@ const HomeScreen = () => {
     <div>
       <h1>Latest products </h1>
       {loading ? (
-        <h2>Loading...</h2>
+        <Loader />
       ) : error ? (
-        <h3>{error}</h3>
+        <Message variant='danger'>{error}</Message>
       ) : (
         <Row>
           {products.map((product) => (

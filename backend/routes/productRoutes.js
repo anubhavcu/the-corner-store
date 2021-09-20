8fe6b472433760e2b04b66e5e230e420c1a6c3c2
anubhavcu <anubhav342y@gmail.com>
2021-09-20 13:39:52 +0530

    added a custom middleware for error handling
    - because if some other unformatted id is hit, it returns back the error
      in HTML format. we want to error to be returned in json format.
    - to override the built in error handler , we pass err along with req,
      res, and next params in the middleware.
    - also the error handler checks for status codes as well. adds 500 if
      some error occurs even the code is 200

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,13 +1,13 @@
-import express from 'express';
+import express from "express";
 const router = express.Router();
-import asyncHandler from 'express-async-handler';
-import Product from '../models/productModel.js';
+import asyncHandler from "express-async-handler";
+import Product from "../models/productModel.js";
 
 // @description  fetch all products
 // @route GET /api/products/
 // @access public
 router.get(
-  '/',
+  "/",
   asyncHandler(async (req, res) => {
     const products = await Product.find({});
     res.json(products);
@@ -18,14 +18,15 @@ router.get(
 // @route GET /api/products/:id
 // @access public
 router.get(
-  '/:id',
+  "/:id",
   asyncHandler(async (req, res) => {
     const product = await Product.findById(req.params.id);
 
     if (product) {
       res.json(product);
     } else {
-      res.status(404).json({ message: 'Product not found !' });
+      res.status(404);
+      throw new Error("Product not found !");
     }
   })
 );

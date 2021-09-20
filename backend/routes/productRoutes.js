849314f8ba7c1a081393c0199524fe73a1c8e288
anubhavcu <anubhav342y@gmail.com>
2021-09-20 08:14:42 +0530

    added product routes
    - productRoutes is handling routes for /api/products/ || and
      /api/products/:id
    - added express-async-handler as a dependency, this basically allows us
      to handle error/exceptions and pass them to express error handlers,
    i.e we don't have to manually try...catch each controller on every route

---
+++ backend/routes/productRoutes.js
@@ -0,0 +1,33 @@
+import express from 'express';
+const router = express.Router();
+import asyncHandler from 'express-async-handler';
+import Product from '../models/productModel.js';
+
+// @description  fetch all products
+// @route GET /api/products/
+// @access public
+router.get(
+  '/',
+  asyncHandler(async (req, res) => {
+    const products = await Product.find({});
+    res.json(products);
+  })
+);
+
+// @description  fetch single product
+// @route GET /api/products/:id
+// @access public
+router.get(
+  '/:id',
+  asyncHandler(async (req, res) => {
+    const product = await Product.findById(req.params.id);
+
+    if (product) {
+      res.json(product);
+    } else {
+      res.status(404).json({ message: 'Product not found !' });
+    }
+  })
+);
+
+export default router;
